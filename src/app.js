a2473eb06003edf3fd553aa77f24f6e9e3969be1
David Almanza <davidalmanza88@gmail.com>
2016-10-23 18:14:04 -0600

    Usar config para el rutear la aplicacion. Create Updata Delete

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
-angular.module('myApp', [])
+angular.module('myApp', ['ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        controller: 'MyController as todoList',
+        templateUrl: '2.html'
+      })
+      .when('/edit/:id', {
+        controller: 'EditController as editCont',
+        templateUrl: '2-edit.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      })
+  })
   .controller('MyController', function () {
     var vm = this;
+
     vm.users = [
       {id:1, name: 'David', age: 28},
       {id:2, name: 'Jose', age: 30}
@@ -18,4 +33,13 @@ angular.module('myApp', [])
         return user.id != userId;
       });
     }
+    var updateClock = function() {
+      vm.clock = new Date();
+    };
+    setInterval(updateClock, 1000);
+  })
+  .controller('EditController', function ($routeParams) {
+    var vm = this;
+    vm.userId = $routeParams.id;
+    console.log('id of', vm.userId);
   });
